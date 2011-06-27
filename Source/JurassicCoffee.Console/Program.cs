4b438f06017fcb868e91cd497fecf7480e274609
christian westman <creamdog@creamdog.se>
2011-06-27 22:31:06 +0200

    added pre/post compile examples

--- Source/JurassicCoffee.Console/Program.cs
+++ Source/JurassicCoffee.Console/Program.cs
@@ -7,18 +7,26 @@ namespace JurassicCoffee.Console
         static void Main(string[] args)
         {
 
-            var compiler = new Core.Compiler(postcompilationSteps: new Func<string, string, string>[] { AddSourceFileName });
+            var compiler = new Core.Compiler(precompilationSteps: new Func<string, string, string>[] { AddCompilationDate }, postcompilationSteps: new Func<string, string, string>[] { AddSourceFileName, RemoveNewLines });
 
             
             compiler.Compile("test.coffee");
 
         }
 
+        private static string AddCompilationDate(string file, string source)
+        {
+            return string.Format("compiledate = '{0}'{1}{2}", DateTime.Now.ToShortDateString(), Environment.NewLine, source);
+        }
 
         private static string AddSourceFileName(string file, string source)
         {
-            source += Environment.NewLine+"//from file: " + file + Environment.NewLine;
-            return source;
+            return string.Format("/*source file: {0}*/{1}{2}", file, source, Environment.NewLine);
+        }
+
+        private static string RemoveNewLines(string file, string source)
+        {
+            return source.Replace(Environment.NewLine, " ").Replace('\n',' ');
         }
     }
 }
